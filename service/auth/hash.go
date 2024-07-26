be5e57014f7dbeb059ed770d3350017d69934260
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-07-26 22:27:11 +0630

    [develop]has password util

---
+++ service/auth/hash.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	return string(hash), nil
+
+}
